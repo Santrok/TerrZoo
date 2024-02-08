95442c53448784c00ede9dfa7502de4c83a6f75d
Flint <amentor256@gmail.com>
2024-02-08 20:21:53 +0300

    create app api

--- config/urls.py
+++ config/urls.py
@@ -43,6 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('main/', include('main.urls')),
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
