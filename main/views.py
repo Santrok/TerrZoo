579fcb76cefe1ca585ac4e1d70db2cec09bbec90
Banywick <android19377@gmail.com>
2024-02-09 13:45:10 +0300

    стилизация формы авторизации

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 
-
+from main.forms import LoginForm
 
 
 # Create your views here.
@@ -11,7 +11,8 @@ def get_page(request):
     return render(request, 'index.html')
 
 def login_view(request):
-    return render(request, 'registration/login.html')
+    login_form = LoginForm()
+    return render(request, 'registration/login.html', {'login_form': login_form})
 
 
 
