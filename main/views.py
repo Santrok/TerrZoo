2a7e53d8566eaf2277649d3e33a49da6098bf87c
Lenapanasyuk <alena0109819@gmail.com>
2024-02-10 14:49:33 +0300

    dumpdata 2

--- main/views.py
+++ main/views.py
@@ -2,13 +2,17 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 
-from main.models import Animal, Product
+from main.models import Animal, Product, Brand
 
 
 # Create your views here.
 
 def get_page(request):
     """"""
+    brands = Brand.objects.all()
+    for brand in brands:
+        print(brand.image)
+    print(brand)
     animals = Animal.objects.all()
     products = list(Product.objects.all())
     popular_product = sorted(products,
@@ -18,8 +22,8 @@ def get_page(request):
                           key=lambda x: x.id,
                           reverse=True)
 
-    for  i in popular_product:
-        print(i.sales_counter)
+    # for i in popular_product:
+        # print(i.sales_counter)
     context = {"animals": animals,
                "popular_products": popular_product,
                "new_products": new_products}
