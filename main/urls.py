ac8cfd7f4ac43c0bb9e9372d3543612d6e7a0464
Lenapanasyuk <alena0109819@gmail.com>
2024-02-11 22:29:01 +0300

    intetmediate version2 catalog

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from main.views import get_page
+from main.views import get_page, get_page_catalog
 
 urlpatterns = [
     path('', get_page),
+    path('catalog/', get_page_catalog)
 
 ]
--- main/views.py
+++ main/views.py
@@ -12,9 +12,6 @@ def get_page(request):
     articals = Article.objects.all()
     reviews = Review.objects.select_related('user').all()
     brands = Brand.objects.all()[0:12]
-    for brand in brands:
-        print(brand.image)
-    # print(brand)
     animals = Animal.objects.all()
     products = list(Product.objects.all())
     popular_product = sorted(products,
@@ -23,9 +20,6 @@ def get_page(request):
     new_products = sorted(products,
                           key=lambda x: x.id,
                           reverse=True)
-
-    # for i in popular_product:
-        # print(i.sales_counter)
     context = {"animals": animals,
                "popular_products": popular_product,
                "new_products": new_products,
@@ -34,4 +28,22 @@ def get_page(request):
                "articals": articals}
     return render(request=request,
                   template_name='index.html',
+                  context=context)
+
+def get_page_catalog(request):
+    animals = Animal.objects.prefetch_related('brand_set','categoryproduct_set')
+    products = Product.objects.all()
+    print('________________________________')
+    for i in animals:
+        print(i.type)
+        for a in i.brand_set.all():
+            print(a.name)
+        for b in i.categoryproduct_set.all():
+            print('@@@',b.name)
+    context = {"animals": animals,
+               "products": products
+
+    }
+    return render(request=request,
+                  template_name='catalog.html',
                   context=context)
