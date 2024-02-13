048da0a8c414293dc33d3df610eb2980470228c6
Oliver1ck <dhsj@yandex.ru>
2024-02-13 21:23:57 +0300

    add basket js and push local storage obj for render basket preview

--- main/static/js/main_page/basket.js
+++ main/static/js/main_page/basket.js
@@ -5,9 +5,21 @@ const headerBottomBasketCount = document.querySelector('.header__bottom-basket >
 
 let count = 0;
 headerBottomBasketCount.textContent = count
+const basketArrayObj = []   
 
 sliderItemBasketBtn.forEach(item => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (e) => {
+        let array = []
+        for(let i of Array.from(e.currentTarget.parentElement.parentElement.children[2].children)){
+            array.push(i.textContent.trim())
+        }
+        basketArrayObj.push({
+            src : e.currentTarget.parentElement.parentElement.children[0].children[0].src,
+            title : e.currentTarget.parentElement.parentElement.children[1].textContent.trim(),
+            weight : array,
+            price:e.currentTarget.parentElement.parentElement.children[3].children[0].textContent.trim()
+        })     
+        localStorage.setItem('basket', JSON.stringify(basketArrayObj));
         count++
         headerBottomBasketCount.textContent = count
     })
