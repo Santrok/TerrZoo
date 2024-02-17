ca8330b339fb8a2143f87dcad4a66f0b8efdac7d
Oliver1ck <dhsj@yandex.ru>
2024-02-17 18:39:37 +0300

    checkbox

--- main/static/js/catalog_page/checkbox.js
+++ main/static/js/catalog_page/checkbox.js
@@ -4,6 +4,7 @@ if(filterItem){
     for(let i of filterItem.children) {
         i?.children[1].classList.replace('filter__type-list-item-label','brand__list-item-label')
         i?.children[0].classList.replace('filter__type-list-item-hide', 'brand__type-list-item-hdie')
+        i?.children[0].setAttribute('type', 'checkbox')
         i?.children[0].setAttribute('name', 'subtype')
     }    
 }
