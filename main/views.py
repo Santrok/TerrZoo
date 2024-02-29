614e3a8fddd73467b8eb3ea7fdf4756ab8e2ac56
Ko100fei <dobysh.k@gmail.com>
2024-02-29 15:08:29 +0300

    изменения в get_order_details_page

--- main/views.py
+++ main/views.py
@@ -530,24 +530,20 @@ def get_profile_subscriptions_page(request):
 def get_order_details_page(request, order_id):
     '''Отдаем страничку с деталями заказа из личного кабинета'''
     order_details = Order.objects.get(id=order_id)
+    order_details.count = 0
     json_obj = order_details.order_item
-    product_list_id = []
-    weight_list = []
-    product_amount = []
-
+    product_list = []
     for i in json_obj:
-        product_list_id.append(i.get('id'))
-        weight_list.append(i.get('weight')[0])
-        product_amount.append(i.get('count'))
-    product_list = Product.objects.filter(id__in=product_list_id)
-    all_products_amount = sum(product_amount)
+        product = Product.objects.filter(id=i.get('id'))
+        add_in_product = product[0]
+        add_in_product.weight = i.get('weight')[0]
+        add_in_product.count = i.get('count')
+        order_details.count += i.get('count')
+        product_list.append(add_in_product)
 
     context = {
         'order_details': order_details,
         'product_list': product_list,
-        'weight_list': weight_list,
-        'product_amount': product_amount,
-        'all_products_amount': all_products_amount,
     }
     return render(request=request,
                   template_name='profile_order_details.html',
