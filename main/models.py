79d6db3fd78449126832ccdcce5dc1f86832cc70
Ko100fei <dobysh.k@gmail.com>
2024-03-11 17:17:05 +0300

    created model OrderForAnonymousUser

--- main/models.py
+++ main/models.py
@@ -341,7 +341,7 @@ class Order(models.Model):
     ]
 
     order_number = models.CharField(verbose_name='Номер заказа',
-                                    max_length=15)
+                                    max_length=16)
     data_create = models.DateTimeField("Время заказа",
                                        auto_now_add=True)
     products = models.ManyToManyField("Product",
@@ -392,7 +392,8 @@ class AdminOrder(admin.ModelAdmin):
 
     def get_readonly_fields(self, request, obj=None):
         if obj and not request.user.is_superuser:
-            return 'order_number', 'user', 'check_order', 'total_price', 'pay_card'
+            return ('order_number', 'user', 'products', 'check_order',
+                    'total_price', 'pay_card', 'order_item', 'order_show')
         return self.readonly_fields
 
 
