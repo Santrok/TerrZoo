cba1fb37ff0d8922052e84f5bd972212e8eaca69
Ko100fei <dobysh.k@gmail.com>
2024-03-18 14:14:10 +0300

    buy one click 1

--- main/static/js/base_template/buy_one_click.js
+++ main/static/js/base_template/buy_one_click.js
@@ -5,3 +5,18 @@
 // basket.splice(basket.length-1, 0, ...data[0].order_item);
 // localStorage.setItem("basket", JSON.stringify(basket));
 //
+
+const buyOneClickBtn = document.querySelector(".buy__one-click > button");
+
+buyOneClickBtn.addEventListener("click", () => {
+    const buyOneClickForm = document.querySelector(".buy__one-click");
+    const nameUser = buyOneClickForm.querySelector("input[name='name']");
+    const phoneUser = buyOneClickForm.querySelector("input[name='phone']");
+    console.log(nameUser);
+    console.log(phoneUser);
+    // const csrfToken = document.getElementsByName("csrfmiddlewaretoken")[0].value;
+    // const data = {
+    //     name_user: nameUser.value,
+    //     phone_number_user: phoneUser.value,
+    // };
+});
