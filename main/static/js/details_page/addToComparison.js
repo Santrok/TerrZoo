cab61467154ade77899ccaf01ec86a4914a63849
Oliver1ck <artem131103@yandex.ru>
2024-03-18 14:48:02 +0300

    fix adding to comparison btn & hearts btn

--- main/static/js/details_page/addToComparison.js
+++ main/static/js/details_page/addToComparison.js
@@ -1,18 +1,15 @@
-document.addEventListener("DOMContentLoaded", addToComparisonFunc);
 const checkID = document.querySelector(".about__product-basket-btn").dataset.productId;
 const comparisonList = JSON.parse(localStorage.getItem("comparisonList")) || [];
 const addComparisonList = document.querySelector(".aboout__product-comprasion");
 
-function addToComparisonFunc() {
-  for (let i of comparisonList) {
-    if (i === checkID) {
-      addComparisonList.textContent = "Удалить из сравнения";
-    }
+for (let i of comparisonList) {
+  if (i === checkID) {
+    addComparisonList.textContent = "Удалить из сравнения";
   }
 }
 
 addComparisonList.addEventListener("click", (e) => {
-  if (addComparisonList.textContent === "Добавить в сравнение") {
+  if (addComparisonList.textContent.trim() === "Добавить в сравнение") {
     comparisonList.push(checkID);
     localStorage.setItem("comparisonList", JSON.stringify(comparisonList));
     addComparisonList.textContent = "Удалить из сравнения";
