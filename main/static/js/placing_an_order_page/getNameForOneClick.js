c519821a473d290e76c9d8a6296365f70b33459b
Oliver1ck <artem131103@yandex.ru>
2024-03-19 17:05:45 +0300

    fixiki

--- main/static/js/placing_an_order_page/getNameForOneClick.js
+++ main/static/js/placing_an_order_page/getNameForOneClick.js
@@ -3,6 +3,6 @@ const nameInput = document.getElementById('name')
 const phoneInput = document.getElementById('phone')
 
 if(nameInput.value === '' && phoneInput.value === '') {
-    nameInput.value = JSON.parse(store).name
-    phoneInput.value = JSON.parse(store).phone
+    nameInput.value = JSON.parse(store)?.name
+    phoneInput.value = JSON.parse(store)?.phone
 }
