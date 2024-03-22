454366672f1ad6d310cdabd730db0c2844fd793a
Oliver1ck <artem131103@yandex.ru>
2024-03-22 18:47:17 +0300

    fixiki

--- main/static/js/base_template/searchinput.js
+++ main/static/js/base_template/searchinput.js
@@ -1,50 +1,58 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Получение элементов для событий
-    const searchInput = document.querySelector("#search__input")
-    const insertResultData = document.querySelector('.search__drop-down')
-    searchInput.addEventListener('input', fetchQueryParams)
-    searchInput.addEventListener('keyup', (e) => getPageCatalog(e, searchInput))
+  // Получение элементов для событий
+  const searchInput = document.querySelector("#search__input");
+  const insertResultData = document.querySelector(".search__drop-down");
+  searchInput.addEventListener("input", fetchQueryParams);
+  searchInput.addEventListener("keyup", (e) => getPageCatalog(e, searchInput));
 
-    function getPageCatalog(e, input) {
-        if (e.code === "Enter") {
-            window.location.href = `${localStorage.getItem('baseUrl')}/search/`
-            localStorage.setItem('searchKeyWord', input.value.trim())
-        }
+
+  function getPageCatalog(e, input) {
+    if (e.code === "Enter") {
+      window.location.href = `${localStorage.getItem("baseUrl")}/search/`;
+      localStorage.setItem("searchKeyWord", input.value.trim());
     }
+  }
 
-    searchInput.addEventListener('focusout', () => {
+  searchInput.addEventListener("focusout", (e) => {
+    console.log(e.target)
+    if(searchInput.value.length < 3){
         insertResultData.style.display = 'none'
         insertResultData.innerHTML = ''
-    })
-    function fetchQueryParams(event) {
-        if (searchInput.value.length >= 3) {
-            fetch(`${localStorage.getItem('baseUrl')}/api/get_search_product/?title=${searchInput.value}`)
-                .then(resp => resp.json())
-                .then(data => {
-                    localStorage.setItem("result_search_objects", JSON.stringify(data.results))
-                    insertResultData.style.display = 'block'
-                    insertResultData.innerHTML = ''
-                    for (let item of data.results) {
-                        insertResultData.innerHTML += `<a class="search__drop-down-item" href=${localStorage.getItem('baseUrl')}/details/${item.id}>
+    }
+  });
+
+  function fetchQueryParams(event) {
+    if (searchInput.value.length >= 3) {
+      fetch(`${localStorage.getItem("baseUrl")}/api/get_search_product/?title=${searchInput.value}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+          localStorage.setItem("result_search_objects", JSON.stringify(data.results));
+
+          insertResultData.style.display = "block";
+          insertResultData.innerHTML = "";
+          for (let item of data.results) {
+            insertResultData.innerHTML += `<a class="search__drop-down-item" href="${localStorage.getItem(
+              "baseUrl"
+            )}/details/${item.id}">
                             <div class="search__drop-down-item-img">
                                 <img src="${item.image_prev}" alt="item">
                             </div>
                             <p class="search__drop-down-item-title">${item.title}</p>
-                        </a>`
-                    }
-                    if(data.results.length === 0) {
-                        insertResultData.innerHTML = `<div class="search__drop-down-item">
+                        </a>`;
+          }
+          if (data.results.length === 0) {
+            insertResultData.innerHTML = `<div class="search__drop-down-item">
                         <p class="search__drop-down-item-title">По вашему запросу ничего не найдено</p>
-                    </в>`
-                    }
-                })
-        } else {
-            let insertResultData = document.querySelector('.search__drop-down')
-            insertResultData.style.display = 'none'
-            insertResultData.innerHTML = 'Ничего не найдено'
-        }
+                    </в>`;
+          }
+        });
+    } else {
+      let insertResultData = document.querySelector(".search__drop-down");
+      insertResultData.style.display = "none";
+      insertResultData.innerHTML = "Ничего не найдено";
     }
-    const inputMob = document.querySelector('.header__up-search input')
-    inputMob.addEventListener('input', fetchQueryParams)
-    inputMob.addEventListener('keyup', (e) => getPageCatalog(e, inputMob))
-})
+  }
+  const inputMob = document.querySelector(".header__up-search input");
+  inputMob.addEventListener("input", fetchQueryParams);
+  inputMob.addEventListener("keyup", (e) => getPageCatalog(e, inputMob));
+});
